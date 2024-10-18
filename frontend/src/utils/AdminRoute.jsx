a2d651b4639f3777f2b28981c42370b40f988c31
khangltt2003 <khangltt2003@gmail.com>
2024-10-18 14:25:51 -0500

    refactor AdminRoute.jsx

--- frontend/src/utils/AdminRoute.jsx
+++ frontend/src/utils/AdminRoute.jsx
@@ -1,8 +1,18 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AuthContext from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const AdminRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user.is_staff) {
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    }
+  }, [user, navigate]);
+
   return user.is_staff ? children : <div className="min-h-screen">You do not have permission to view this page</div>;
 };
 
